1934e8b0d6aef86d5f559aceb32d8d2d3317d8a7
geberi <126200667+geberich@users.noreply.github.com>
2023-03-12 18:46:34 +0200

    Add files via upload

---
+++ js/common.js
@@ -0,0 +1,35 @@
+$(function() {
+
+	//Chrome Smooth Scroll
+	try {
+		$.browserSelector();
+		if($("html").hasClass("chrome")) {
+			$.smoothScroll();
+		}
+	} catch(err) {
+
+	};
+
+	// replace custom forms
+
+
+	$('a.btn, .nav a').on('click', function(event){
+		event.preventDefault();
+		var hr = $(this).attr('href');
+		$('body, html').animate({scrollTop : $(hr).offset().top} ,1000);
+	})
+
+	$(".toggle_mnu").click(function() {
+		$(".sandwich").toggleClass("active");
+		$('.nav').toggle();
+	});
+
+	var ww = $(window).width();
+	if(ww < 950){
+		$('.nav a').on('click', function(event){
+			$('.nav').hide();
+			$(".sandwich").removeClass("active");
+		});
+	}
+
+});
